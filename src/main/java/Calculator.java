edb3553c6c4eeb97e44568ea219eac6ce0529680
Alex_Z-O <zorin.ak@yandex.ru>
2025-11-23 18:14:13 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,20 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b == 0){
+            throw new IllegalArgumentException("Нельзя делить на 0");
+        }
+        return a / b;
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return (int) Math.pow(a, b);
     }
     public int solver(){
-        //TODO inser your realisation in method solver
+        return times(23,4) + div(6,2) - add(4,125) * dif(438,435);
     }
 }
